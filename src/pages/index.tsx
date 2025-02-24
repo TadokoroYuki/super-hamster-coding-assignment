343452c37627b1469f8743282af332e769a2ab78
田所佑基 <tdkryk@icloud.com>
2025-02-24 23:13:26 +0900

    ボタンコンポーネントの完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Button } from "@/components/button";
+import { FiEdit } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <Button />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        padding: "2em",
+      }}
+    >
+      <h1>Button Component Demo</h1>
+
+      <Button label="編集" variant="black" iconLeft={<FiEdit />} />
+      <Button label="編集" variant="border" iconLeft={<FiEdit />} />
+      <Button label="編集" variant="white" iconLeft={<FiEdit />} />
+    </div>
   );
 }
